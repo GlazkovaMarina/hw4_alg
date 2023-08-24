e8e40a94a553ed00c3e23328f9e2a916bf9971d4
Glazkova_Marina <glazkova.mv@bk.ru>
2023-08-24 14:42:50 +0300

    add hw4

---
+++ Main.java
@@ -0,0 +1,17 @@
+package hw4.Main;
+
+public class Main {
+    public static void main(String[] args) {
+        RedBlackTree tree = new RedBlackTree();
+        tree.add(3);
+        tree.add(8);
+        tree.add(25);
+        tree.add(12);
+        tree.add(1);
+        tree.add(2);
+        tree.add(26);
+        tree.add(27);
+        
+    }
+    
+}
